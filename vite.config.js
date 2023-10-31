195c355214174211a462073584a33716cf7b826a
Beginners-Luck123 <dynieme.fms@gmail.com>
2023-10-30 20:39:07 -0400

    edited vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "/DynieM.github.io/",
-	plugins: [react()]
+	base: "",
+	plugins: [react()],
+	  build: {
+    outDir: 'build',
+  },
 });
