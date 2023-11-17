886f6fbf1486c7824b46024e8930fa7dfb045690
Beginners-Luck123 <dynieme.fms@gmail.com>
2023-11-17 14:36:16 -0500

    added project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -53,6 +53,16 @@ const Projects = () => {
 							React.js and Tailwind. Designed and prototyped on Figma.
 						</p>
 					</a>
+					<a className="block w-full max-w-sm p-6 m-4 border-2 border-black rounded-lg rounded-lg shadow bg-white hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 ">
+						<h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+							Seeking Citizenship
+						</h5>
+						<p className="font-normal text-gray-700 dark:text-gray-400">
+							Developed a web-based application using HTML, CSS, JS, and
+							Bootstrap as the framework that simplifies the citizenship
+							process.
+						</p>
+					</a>
 					<img
 						src={assets.RedBlob}
 						alt="Red Blob"
